9146faef09f27e7b1a6aaa70a17f2fbda4fc3a14
diegooliveira <diego.oliveira.am@gmail.com>
2021-01-14 16:45:21 -0400

    sucos toolbar

--- app/src/main/java/com/diego/cardapiorestaurante/Sucos.kt
+++ app/src/main/java/com/diego/cardapiorestaurante/Sucos.kt
@@ -1,12 +1,22 @@
 package com.diego.cardapiorestaurante
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class Sucos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sucos)
         supportActionBar!!.hide()
+        toolbar.title="Sucos"
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_back))
+        toolbar.setNavigationOnClickListener {
+            var intent = Intent( this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
